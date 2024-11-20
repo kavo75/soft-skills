c42bd39301a7d2c8e3cfad64e903abf5aa09d2d8
kavo <karelvo@icloud.com>
2024-11-20 19:13:41 +0100

    Set up cvless theme and site structure

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
-document.getElementById('down-arrow').addEventListener('click', function() {
-    window.scrollTo({
-        top: document.getElementById('section2').offsetTop,
-        behavior: 'smooth'
+document.addEventListener("DOMContentLoaded", function() {
+  const glideButton = document.getElementById("glideButton");
+  if (glideButton) {
+    glideButton.addEventListener("click", function() {
+      document.querySelector("#section2").scrollIntoView({ behavior: "smooth" });
     });
+  }
 });
